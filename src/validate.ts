0e53e24fbf988ee245cdd0da0145ea62c3a51f45
salman@koii.network <meerahmeddev@gmail.com>
2023-08-15 22:33:31 +0500

    validations fix

--- src/validate.ts
+++ src/validate.ts
@@ -1,3 +1,4 @@
+import Joi from "joi";
 //import { Web3Storage } from "web3.storage";
 
 interface Task {
@@ -41,224 +42,82 @@ enum RequirementType {
   ARCHITECTURE = "ARCHITECTURE",
   OS = "OS",
 }
+const requirementTagSchema = Joi.object({
+  type: Joi.string()
+    .valid(...Object.values(RequirementType))
+    .required(),
+  value: Joi.alternatives()
+    .try(Joi.string(), Joi.array().items(Joi.string()))
+    .optional(),
+  description: Joi.string().optional(),
+});
+const taskMetadataSchema = Joi.object({
+  author: Joi.string().required(),
+  description: Joi.string().required(),
+  repositoryUrl: Joi.string().uri().required(),
+  createdAt: Joi.number().required(),
+  imageUrl: Joi.string().allow('').optional().uri(),
+  requirementsTags: Joi.array().items(requirementTagSchema).optional(),
+});
+const taskSchema = Joi.object({
+  task_name: Joi.string().required(),
+  task_executable_network: Joi.string()
+    .valid("DEVELOPMENT", "ARWEAVE", "IPFS")
+    .required(),
+  secret_web3_storage_key: Joi.string().required(),
+  task_audit_program: Joi.string().allow('').min(1).when("task_executable_network", {
+    is: "IPFS",
+    then: Joi.required(),
+    otherwise: Joi.optional(),
+  }),
+  task_audit_program_id: Joi.string()
+    .when("task_executable_network", {
+      is: Joi.valid("DEVELOPMENT", "ARWEAVE"),
+      then: Joi.required(),
+      otherwise: Joi.optional(),
+    })
+    .max(64),
+  round_time: Joi.number().required(),
+  audit_window: Joi.number().required(),
+  submission_window: Joi.number().required(),
+  minimum_stake_amount: Joi.number().required(),
+  total_bounty_amount: Joi.number()
+    .required()
+    .min(10)
+    .message("Total bounty amount cannot be less than 10 KOII"),
+  bounty_amount_per_round: Joi.number()
+    .required()
+    .max(Joi.ref("total_bounty_amount")),
+  allowed_failed_distributions: Joi.number().required(),
+  space: Joi.number()
+    .required()
+    .min(1)
+    .message("Space cannot be less than 1 mb"),
+});
 
-async function main(metaData: TaskMetadata, task: Task) {
-  const error: any = {};
-  const {
-    author,
-    description,
-    repositoryUrl,
-    createdAt,
-    imageUrl,
-    requirementsTags,
-  } = metaData;
+function main(metaData: TaskMetadata, task: Task) {
+  let isValid = true;
 
-  // Check that author is a non-empty string
-  if (typeof author !== "string" || author.trim().length === 0) {
-    error["author"] = "Missing";
+  const validatedTask = taskSchema.validate(task, { abortEarly: false });
+  if (validatedTask.error) {
+    isValid = false;
+    console.log(
+      validatedTask.error.details.map((detail) => detail.message).join(", ")
+    );
+  }
+  const validatedTaskMetadata = taskMetadataSchema.validate(metaData, {
+    abortEarly: false,
+  });
+  if (validatedTaskMetadata.error) {
+    isValid = false;
+    console.log(
+      validatedTaskMetadata.error.details
+        .map((detail) => detail.message)
+        .join("\n ")
+    );
   }
 
-  // Check that description is a non-empty string
-  if (typeof description !== "string" || description.trim().length === 0) {
-    error["description"] = "Missing";
-  }
-
-  // Check that repositoryUrl is a non-empty string
-  if (typeof repositoryUrl !== "string" || repositoryUrl.trim().length === 0) {
-    error["repositoryUrl"] = "Missing";
-  }
-
-  // Check that description is a non-empty string
-  if (typeof createdAt !== "number" || description.trim().length === 0) {
-    error["createdAt"] = "Missing";
-  }
-  
-  // Check that requirementsTags is an array of non-empty strings
-  if (requirementsTags !== undefined) {
-    const result: boolean = validateRequirementsTags(requirementsTags);
-    if (!result) {
-      error["requirementsTags"] =
-        "tags do not have valid types or the values specified are not acceptable";
-    }
-  }
-
-  // Validating task parameters
-
-  if (!task.secret_web3_storage_key) {
-    error["secret_web3_storage_key"] =
-      "Please specify the web3.storage secret key in YML";
-  }
-
-  if (!task.task_executable_network) {
-    error["task_executable_network"] = "Missing";
-  }
-
-  // Check if all required properties are present and have valid values
-
-  if (
-    !task.task_name ||
-    !task.task_executable_network ||
-    !task.secret_web3_storage_key ||
-    !task.round_time ||
-    !task.audit_window ||
-    !task.submission_window ||
-    !task.minimum_stake_amount ||
-    !task.total_bounty_amount ||
-    !task.allowed_failed_distributions ||
-    !task.space
-  ) {
-    error["taskParams"] = "task parameter missing";
-  }
-
-  // task_name cannot be greater than 24  characters
-  if (task.task_name.length > 24) {
-    error["task_name"] = "cannot be more than 24 characters long";
-  }
-
-  // verify web3.storage key
-
-  if (task.secret_web3_storage_key.length < 200) {
-    error["secret_web3_storage_key"] = "cannot be less than 200 characters";
-  }
-
-  //   const verify: boolean = await validateSecretKey(task.secret_web3_storage_key);
-  //   if (!verify) {
-  //     error["secret_web3_storage_key"] = "Invalid key";
-  //   }
-  if (
-    task.task_executable_network !== "DEVELOPMENT" &&
-    task.task_executable_network !== "ARWEAVE" &&
-    task.task_executable_network !== "IPFS"
-  ) {
-    // Check if task_executable_network is a valid value
-    error["task_executable_network"] =
-      "Unacceptable values. Network value can only be DEVELOPMENT , ARWEAVE or IPFS";
-  }
-
-  // Check if all properties have valid types and values
-  if (
-    typeof task.task_name !== "string" ||
-    typeof task.secret_web3_storage_key !== "string" ||
-    typeof task.round_time !== "number" ||
-    typeof task.audit_window !== "number" ||
-    typeof task.submission_window !== "number" ||
-    typeof task.minimum_stake_amount !== "number" ||
-    typeof task.total_bounty_amount !== "number" ||
-    typeof task.bounty_amount_per_round !== "number" ||
-    typeof task.allowed_failed_distributions !== "number" ||
-    typeof task.space !== "number" ||
-    task.round_time <= 0 ||
-    task.audit_window <= 0 ||
-    task.submission_window <= 0 ||
-    task.minimum_stake_amount <= 0 ||
-    task.total_bounty_amount <= 0 ||
-    task.allowed_failed_distributions < 0 ||
-    task.space <= 0
-  ) {
-    error["taskParams"] = "type error in task params";
-  }
-
-  // Check audit properties
-
-  if (
-    task.task_executable_network == "DEVELOPMENT" ||
-    task.task_executable_network == "ARWEAVE"
-  ) {
-    if (!task.task_audit_program_id) {
-      error["task_audit_program_id"] = "Missing";
-    } else if (task.task_audit_program_id.length > 64) {
-      error["task_audit_program_id"] = "cannot be more than 64 characters";
-    }
-  }
-  if (task.task_executable_network == "IPFS") {
-    if (!task.task_audit_program) {
-      error["task_audit_program"] = "Missing";
-    }
-  }
-
-  // check the rewards allocated
-  if (task.total_bounty_amount < 10) {
-    error["total_bounty_amount"] =
-      "Total bounty amount cannot be less than 10 KOII";
-  }
-
-  if (task.bounty_amount_per_round > task.total_bounty_amount) {
-    error["bounty_amount_per_round"] =
-      "cannot be more than total bounty amount";
-  }
-
-  if (task.space < 1) {
-    error["space"] = "cannot be less than 1 mb";
-  }
-
-  if (!isObjectEmpty(error)) {
-    console.error("\x1b[31m", `ERROR ${JSON.stringify(error)}`);
-    console.log("\x1b[30m", "Please resolve these issues");
-    process.exit();
-  }
-}
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-function isObjectEmpty(obj: object): boolean {
-  for (const key in obj) {
-    // eslint-disable-next-line no-prototype-builtins
-    if (obj.hasOwnProperty(key)) {
-      return false;
-    }
-  }
-  return true;
-}
-
-// async function validateSecretKey(secretKey: string): Promise<boolean> {
-//   try {
-//     const client = new Web3Storage({ token: secretKey });
-//     // make a test call to the API to ensure the secret key is valid
-//     const response = await client.list();
-//     if (response) {
-//       return true;
-//     }
-//     return false;
-//   } catch (error) {
-//     console.error(`Error validating secret key: ${error}`);
-//     return false;
-//   }
-// }
-
-function validateRequirementsTags(requirementsTags: RequirementTag[]): boolean {
-  for (const tag of requirementsTags) {
-    if (!tag.type || !(tag.type in RequirementType)) {
-      //console.error(`Invalid requirement tag: ${JSON.stringify(tag)}`);
-      return false;
-    }
-    switch (tag.type) {
-      case RequirementType.GLOBAL_VARIABLE:
-      case RequirementType.TASK_VARIABLE:
-        if (tag.value && typeof tag.value !== "string") {
-          console.error(
-            `Invalid value for ${tag.type} requirement: ${tag.value}`
-          );
-          return false;
-        }
-        break;
-      case RequirementType.CPU:
-      case RequirementType.RAM:
-      case RequirementType.STORAGE:
-      case RequirementType.NETWORK:
-      case RequirementType.ARCHITECTURE:
-      case RequirementType.OS:
-        if (!tag.value || typeof tag.value !== "string") {
-          console.error(
-            `Invalid value for ${tag.type} requirement: ${tag.value}`
-          );
-          return false;
-        }
-        break;
-      default:
-        console.error(`Unknown requirement type: ${tag.type}`);
-        return false;
-    }
-  }
-  return true;
+  return !isValid ? process.exit(1) : true;
 }
 
 export default main;
