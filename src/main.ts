fe6c5687c6df1af0275a745b5e4e29244cb71514
Pablo Poggio <pablopoggiog@gmail.com>
2025-05-20 17:41:19 -0300

    Update the portal URL

--- src/main.ts
+++ src/main.ts
@@ -254,7 +254,7 @@ async function main() {
               
               console.log(chalk.yellow('You can get KOII in several ways:'));
               console.log(`  1. ${chalk.green('Purchase directly')} with credit/debit card or Cash App Pay:`);
-              console.log(`     ${chalk.cyan(`https://dev-green-funding-portal.vercel.app/?pub=${payerWallet.publicKey.toBase58()}`)}\n`);
+              console.log(`     ${chalk.cyan(`https://pay.koii.network/?pub=${payerWallet.publicKey.toBase58()}`)}\n`);
               
               console.log(`  2. ${chalk.green('Trade on exchanges')}:`);
               console.log(`     • Gate.io: ${chalk.cyan('https://www.gate.io/trade/KOII_USDT')}`);
